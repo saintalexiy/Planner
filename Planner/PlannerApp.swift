d8683a2422df29b2329b79dff1343d29f8ed7226
Aleksei Rybakov <saintalexiy@gmail.com>
2024-02-24 22:08:19 +0300

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Alex Rybakov on 24.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
